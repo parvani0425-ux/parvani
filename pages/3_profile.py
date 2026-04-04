af90f5b7675b4e5eebc8108e94bde5fca3ccea85
parvani0425-ux <parvani0425@gmail.com>
2026-04-04 22:06:45 +0530

    Update 3_profile.py

--- pages/3_profile.py
+++ pages/3_profile.py
@@ -1,1 +1,17 @@
+import streamlit as st
+import json
 
+st.title("👤 Profile & History")
+
+try:
+    history = json.load(open("history.json"))
+except:
+    history = []
+
+if history:
+    st.subheader("📂 Previously Analyzed Files")
+
+    for file in history[::-1]:
+        st.write("📄", file)
+else:
+    st.info("No history yet")
