88f9b20c75291bdd4b50f1f5a0dbf7dbaafe75ab
parvani0425-ux <parvani0425@gmail.com>
2026-04-04 22:37:03 +0530

    Update 2_Dashboard.py

--- pages/2_Dashboard.py
+++ pages/2_Dashboard.py
@@ -9,7 +9,18 @@ from sklearn.metrics import r2_score
 if not st.session_state.get("logged_in", False):
     st.warning("Please login first")
     st.stop()
-    
+
+st.sidebar.title("🚀 AI Platform")
+
+st.sidebar.page_link("app.py", label="Home")
+st.sidebar.page_link("pages/2_Dashboard.py", label="Dashboard")
+st.sidebar.page_link("pages/3_Profile.py", label="Profile")
+st.sidebar.page_link("pages/4_About.py", label="About")
+
+if st.sidebar.button("Logout"):
+    st.session_state.logged_in = False
+    st.switch_page("app.py")
+
 st.set_page_config(page_title="Dashboard", layout="wide")
 
 st.title("📊 AI Data Analytics Dashboard")
