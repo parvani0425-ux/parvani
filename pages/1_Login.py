34f55adbb0a7c01c7a1294e7ff83054b1c15021c
parvani0425-ux <parvani0425@gmail.com>
2026-04-04 22:35:51 +0530

    Update 1_Login.py

--- pages/1_Login.py
+++ pages/1_Login.py
@@ -1,37 +1,29 @@
 import streamlit as st
-import json
 
-def load_users():
-    try:
-        return json.load(open("users.json"))
-    except:
-        return {}
+st.set_page_config(page_title="Login", layout="wide")
 
-def save_users(users):
-    json.dump(users, open("users.json","w"))
+# INIT SESSION
+if "logged_in" not in st.session_state:
+    st.session_state.logged_in = False
 
 st.title("🔐 Login / Sign Up")
 
-users = load_users()
+tab1, tab2 = st.tabs(["Login", "Sign Up"])
 
-tab1, tab2 = st.tabs(["Login","Signup"])
-
+# LOGIN
 with tab1:
-    u = st.text_input("Username")
-    p = st.text_input("Password", type="password")
+    user = st.text_input("Username")
+    pwd = st.text_input("Password", type="password")
 
     if st.button("Login"):
-        if u in users and users[u]==p:
-            st.session_state["user"] = u
-            st.success("Login successful")
-        else:
-            st.error("Wrong credentials")
+        st.session_state.logged_in = True
+        st.switch_page("pages/2_Dashboard.py")
 
+# SIGNUP
 with tab2:
-    u = st.text_input("New Username")
-    p = st.text_input("New Password", type="password")
+    new_user = st.text_input("Create Username")
+    new_pwd = st.text_input("Create Password", type="password")
 
-    if st.button("Create Account"):
-        users[u]=p
-        save_users(users)
-        st.success("Account created")
+    if st.button("Sign Up"):
+        st.success("Account created! Please login.")
+        
