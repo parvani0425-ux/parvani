b88d1a92cfc347be35e3021e086a79d16bd47ccc
parvani0425-ux <parvani0425@gmail.com>
2026-04-04 22:36:07 +0530

    Update 2_Dashboard.py

--- pages/2_Dashboard.py
+++ pages/2_Dashboard.py
@@ -6,6 +6,10 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import r2_score
 
+if not st.session_state.get("logged_in", False):
+    st.warning("Please login first")
+    st.stop()
+    
 st.set_page_config(page_title="Dashboard", layout="wide")
 
 st.title("📊 AI Data Analytics Dashboard")
