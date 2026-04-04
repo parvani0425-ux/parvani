31fccb0b8388a8fa8c1f2d2089be9e07bfcd210d
parvani0425-ux <parvani0425@gmail.com>
2026-04-04 20:49:18 +0530

    Update 3_About.py

--- pages/3_About.py
+++ pages/3_About.py
@@ -1,1 +1,18 @@
+import streamlit as st
 
+st.title("📖 About")
+
+st.write("""
+This AI Dashboard allows users to:
+
+- Clean data automatically  
+- Visualize trends  
+- Generate insights  
+- Predict outcomes using ML  
+
+Built using:
+- Streamlit  
+- Pandas  
+- Plotly  
+- Scikit-learn  
+""")
